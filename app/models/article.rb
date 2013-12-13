e285ceacdb3b34964314087d68dcf0cea9b46994
Rui Queir¢s <ruifqueiros@gmail.com>
2013-12-13 15:11:58 +0000

    imagens on

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,3 @@
 class Article < ActiveRecord::Base
   belongs_to :user
-
-  has_attached_file :picture,
-                    :path => "public/system/:class/:id/:filename",
-                    :url => "/system/:class/:id/:basename.:extension"
 end
