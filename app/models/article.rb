b43e08fec1d1192350d97a1998fe4ff902e1d780
Rui Queir¢s <ruifqueiros@gmail.com>
2013-12-13 01:58:44 +0000

    ainda nao funciona esta putice

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :picture
-
+  has_attached_file :picture,
+                    :path => "public/system/:class/:id/:filename",
+                    :url => "/system/:class/:id/:basename.:extension"
 end
