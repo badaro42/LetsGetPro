8f83c755053a2874b715642337913d38da41bdaa
Rui Queir¢s <ruifqueiros@gmail.com>
2013-12-14 17:27:54 +0000

    validações

--- app/models/cv_entry.rb
+++ app/models/cv_entry.rb
@@ -3,4 +3,15 @@ class CvEntry < ActiveRecord::Base
   has_many :tasks
   has_one :company
 
+  validates :start_date, presence: true, if: :date_validation?
+
+  def date_validation?
+    if self[:end_date] < self[:start_date]
+      errors[:end_date] << "End Date needs to be higher then Start Date"
+      return false
+    else
+      return true
+    end
+  end
+
 end
